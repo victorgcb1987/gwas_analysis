785ea7f3468ef73d6cf7c6338cc41a06d27e5df7
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2023-05-12 14:26:47 +0200

    Update remove_linkage_disiquilibrium.py

--- remove_linkage_disiquilibrium.py
+++ remove_linkage_disiquilibrium.py
@@ -36,7 +36,7 @@ def parse_arguments():
                         type=int, help=help_step_size,
                         default=5)
     help_bad_ld = "(Optional) allow --bad-ld in plink."
-    parser.add_argument("--bad-ld", type=bool, 
+    parser.add_argument("--bad-ld", "-b", type=bool, 
                         action="store_true", help=help_bad_ld)
     return parser
 
